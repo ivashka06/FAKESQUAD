78ad80b424fcf937f7b9917308cbfeb2a0f77a25
ivashka06 <ivashka0706@gmail.com>
2021-12-23 19:22:29 +0300

    Initial commit

---
+++ FAKESQUAD/MeetManager.cs
@@ -0,0 +1,17 @@
+﻿using FAKESQUAD.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAKESQUAD
+{
+    public class MeetManager
+    {
+        List<Meet> meets = new List<Meet>();
+        public void AddNewMeet(string name, DateTime meetTime)
+        {
+            Meet newMeet = new Meet() { Name = name, CreateTime = DateTime.Now, MeetTime = meetTime};
+            meets.Add(newMeet);
+        }
+    }
+}
---
+++ FAKESQUAD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FAKESQUAD
+{
+    class Program
+    {
+        private static MeetManager meetManager;
+        static void Main(string[] args)
+        {
+            meetManager = new MeetManager();
+
+            Console.Write("Введите имя встречи");
+            string user_nameMeet = Console.ReadLine();
+
+            Console.Write("Введите год встречи");
+            int user_meetTimeYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите месяц встречи");
+            int user_meetTimeMonth = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите день встречи");
+            int user_meetTimeDay = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите час встречи");
+            int user_meetTimeHour = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите минуты встречи");
+            int user_meetTimeMinute = int.Parse(Console.ReadLine());
+
+            DateTime user_meetTime = new DateTime(user_meetTimeYear, user_meetTimeMonth, user_meetTimeDay, user_meetTimeHour, user_meetTimeMinute,0);
+
+            meetManager.AddNewMeet(user_nameMeet, user_meetTime);
+
+            Console.ReadKey();
+        }
+    }
+}
